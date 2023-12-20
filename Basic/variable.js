b2f52356f86587d04460ee9acf00da7cadc6b2cf
sayantan <sayantan.roy123789@gmail.com>
2023-12-21 00:36:20 +0530

    variables

---
+++ Basic/variable.js
@@ -0,0 +1,9 @@
+let myId=15564;
+var email="sayantangoogle.com";
+const city="Burdwan";
+// city="Mumbai";
+console.log(email);
+console.log(city);
+email="sayantanapple.com";
+console.log(email)
+console.table([myId, email,city ])
