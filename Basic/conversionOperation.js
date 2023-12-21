8f1cce86bfa665123b9eb23e98c4b3f0e522ce51
sayantan <sayantan.roy123789@gmail.com>
2023-12-22 01:19:25 +0530

    conversion

---
+++ Basic/conversionOperation.js
@@ -0,0 +1,28 @@
+let score="30"
+console.log(typeof score)
+
+
+let c="25abc"
+console.log(typeof c)
+
+let d=Number(c)
+console.log(typeof d)
+console.log(d)
+
+let x=null
+console.log(x)
+let y=Number(x)
+console.log(y)
+
+let x1;
+let y1=Number(x1)
+console.log(y1)
+
+let islogedIn=1;
+let booleanislogedIn=Boolean(islogedIn)
+console.log(booleanislogedIn)
+
+let h=33
+let k=String(h)
+console.log(k)
+console.log(typeof k)
