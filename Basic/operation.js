98d29910ce934d28fb0abd87fb68d1cf89ac34a0
sayantan <sayantan.roy123789@gmail.com>
2023-12-22 01:46:24 +0530

    operation

---
+++ Basic/operation.js
@@ -0,0 +1,17 @@
+console.log("1"+2) //12
+console.log(1+"2"+"2") //122
+console.log(1+2+'2') //32
+
+console.log(true) //true
+console.log(+true) //1
+console.log(" ") //void
+console.log(+"") //0
+
+let count=100
+count++
+console.log(count);//101
+let val=60
+++val;
+console.log(val) //61
+console.log(++val) //62
+console.log(val++) //62 not increasing
