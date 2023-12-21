e735c27cf8cbe367b418e4a37dc9a9bb53479933
sayantan <sayantan.roy123789@gmail.com>
2023-12-22 02:06:39 +0530

    compresion op

---
+++ Basic/conversion.js
@@ -0,0 +1,15 @@
+console.log("5">2) //true
+console.log("1"<"6")// true
+console.log("2"==2) //true
+console.log("5"===5) //false
+console.log("02">10);//false
+
+console.log(null==0) //false
+console.log(null>0) //false
+console.log(null>=0) //true in js compresion operator convert null to 0.
+console.log(0>=null) //true
+
+console.log(undefined==0) //false
+console.log(undefined>0); //false
+console.log(undefined<0) //false
+
