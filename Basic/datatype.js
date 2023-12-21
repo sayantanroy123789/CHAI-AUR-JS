fa6b0172881db4802579b12d46d2855ff1f61520
sayantan <sayantan.roy123789@gmail.com>
2023-12-22 02:23:25 +0530

    more

--- Basic/datatype.js
+++ Basic/datatype.js
@@ -7,3 +7,19 @@ console.log(ro)
 console.log(typeof age)
 console.log(typeof no)  //undediende
 console.log(typeof null) //object
+
+/* 
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
