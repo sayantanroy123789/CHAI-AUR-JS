026c6d3e1c3d00c9dc75aa897837968644aefc2b
sayantan <sayantan.roy123789@gmail.com>
2024-01-10 02:33:38 +0530

    MATH

---
+++ Basic/Math.js
@@ -0,0 +1,14 @@
+console.log(Math);
+console.log(Math.abs(-5));//5
+console.log(Math.round(5.8));//6
+console.log(Math.ceil(8.2));//9
+console.log(Math.floor(7.9));//7
+console.log(Math.min(6,8,2,9));//2
+console.log(Math.max(6,8,2,9));//9
+
+console.log(Math.random());//alwys belong in 0 to 1.
+console.log(Math.floor((Math.random()*10)+1));
+
+let min=10;
+let max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
