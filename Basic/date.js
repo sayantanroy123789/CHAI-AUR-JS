baf3f0976f9ca0d529010f45381fcb3aefafdcf4
sayantan <sayantan.roy123789@gmail.com>
2024-01-11 02:24:15 +0530

    date

---
+++ Basic/date.js
@@ -0,0 +1,10 @@
+let myDate= new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+console.log(typeof myDate);//object
+
+let mycreate=new Date();
+console.log(mycreate.toLocaleDateString());
